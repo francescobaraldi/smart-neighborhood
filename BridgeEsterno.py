4847e2d409b6667632c2c61bdbfffa611676fa1f
Francesco Baraldi <francesco_baraldi@icloud.com>
2021-12-04 11:39:16 +0100

    Added file for external bridge

--- BridgeEsterno.py
+++ BridgeEsterno.py
@@ -1,9 +1,7 @@
 import serial
 import serial.tools.list_ports
-import numpy as np
-import paho.mqtt.client as mqtt
 
-class Bridge():
+class BridgeEsterno():
     
     def __init__(self, portname: str = None, port_description: str = "arduino", frequency: int = 9600):
         self.portname = portname
@@ -15,36 +13,37 @@ class Bridge():
                 if self.port_description.lower() in port.description.lower():
                     self.portname = port.device
         if self.portname is None:
-            print(f"Connection failed: port {self.port_description} not found")
+            print(f"Connection failed: port {self.port_description} not found.")
             raise Exception
         try:
             self.serial = serial.Serial(self.portname, frequency, timeout=0)
         except:
-            print(f"Connection failed: port {self.port_description} not found")
+            print(f"Connection failed: port {self.port_description} not found.")
             raise Exception
-        print("Connecting to " + self.portname)
+        print(f"Connecting to {self.portname}")
         self.inbuffer = []
     
 
     def loop(self):
         while (True):
             if self.serial is not None:
-                if self.ser.in_waiting>0:
+                if self.serial.in_waiting>0:
                     lastchar=self.serial.read(1)
                     if lastchar==b'\xfe':
-                        print("\nValue received")
-                        self.useData()
+                        print("Value received")
+                        self.process_data()
                         self.inbuffer =[]
                     else:
                         self.inbuffer.append(lastchar)
 
-    def useData(self):
-        if len(self.inbuffer)<3:
-            return False
-        if self.inbuffer[0] != b'\xff':
+
+    def process_data(self):
+        if len(self.inbuffer) < 3 or self.inbuffer[0] != b'\xff':
+            print("Errore: il pacchetto ricevuto non ha un formato corretto.")
             return False
         numval = int.from_bytes(self.inbuffer[1], byteorder='little')
         if numval != 3:
+            print("Errore: il pacchetto ricevuto non ha i dati corretti.")
             return False
         potentiometer_value = int.from_bytes(self.inbuffer[2], byteorder='little')
         photoresistor_value = int.from_bytes(self.inbuffer[3], byteorder='little')
@@ -53,6 +52,7 @@ class Bridge():
         print("Photoresistor value: " + str(photoresistor_value))
         print("Thermometer value: " + str(thermometer_value))
 
+
 if __name__ == '__main__':
-    bridge = Bridge()
+    bridge = BridgeEsterno()
     bridge.loop()
