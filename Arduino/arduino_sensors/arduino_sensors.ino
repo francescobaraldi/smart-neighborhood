fe1ebfa0c7232535dd41e8273aa6c21e573a3c99
Matteo Pagliani <64635883+MatteoPagliani@users.noreply.github.com>
2022-03-14 13:05:55 +0100

    Update arduino_sensors.ino
    Removed temperature sensor, added push button for sud and nord windows

--- Arduino/arduino_sensors/arduino_sensors.ino
+++ Arduino/arduino_sensors/arduino_sensors.ino
@@ -1,11 +1,14 @@
 const int potentiometer_pin = A0;
 const int photoresistor_pin = A1;
 const int thermometer_pin = A2;
+const int button_pin_sud = 2; // azionamento manuale scuri finestra sud
+const int button_pin_nord = 3; // azionamento manuale scuri finestra nord
 
 int potentiometer_value;
 int photoresistor_value;
 int thermometer_value;
-float thermometer_degrees;
+bool button_value_sud;
+bool button_value_nord;
 
 unsigned long lasttime;
 
@@ -15,6 +18,8 @@ void setup(){
   pinMode(potentiometer_pin, INPUT);
   pinMode(photoresistor_pin, INPUT);
   pinMode(thermometer_pin, INPUT);
+  pinMode(button_pin_sud, INPUT);
+  pinMode(button_pin_nord, INPUT);
   lasttime = millis();
 }
 
@@ -25,13 +30,15 @@ void loop(){
     potentiometer_value = analogRead(potentiometer_pin); // controllare la velocità del vento: deltaValue ogni 5 secondi -> possibile deltaValue = 25
     photoresistor_value = analogRead(photoresistor_pin); // soglia = 100 (selezionare uint8 in realterm)
     thermometer_value = analogRead(thermometer_pin);
-    therm<ometer_degrees = ((thermometer_value / 1024) * 5 - 0.5) * 100;
-
+    button_value_sud = digitalRead(button_pin_sud); // HIGH = button pressed (--> cambia stato della finestra sud), LOW = button released
+    button_value_nord = digitalRead(button_pin_nord); // HIGH = button pressed (--> cambia stato della finestra nord), LOW = button released
+    
     Serial.write(0xff);
-    Serial.write(0x03); // Lunghezza payload in byte
+    Serial.write(0x04); // Lunghezza payload in byte
     Serial.write((char)(map(potentiometer_value, 0, 1024, 0, 253)));  // Da 0 a 253 e non 255 perchè 254 e 255 (fe e ff) servono per l'header e il footer del pacchetto
     Serial.write((char)(map(photoresistor_value, 0, 1024, 0, 253)));
-    Serial.write(thermometer_degrees);
+    Serial.write((char)(map(button_value_sud, 0, 1, 0, 253)));
+    Serial.write((char)(map(button_value_nord, 0, 1, 0, 253)));
     Serial.write(0xfe);
   }
 }
