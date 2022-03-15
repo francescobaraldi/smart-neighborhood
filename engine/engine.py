d42c4c7e8781bcf1d73bee88b6c9544cee27cd6d
Francesco Baraldi <francesco_baraldi@icloud.com>
2022-03-15 16:13:27 +0100

    Update engine

--- engine/engine.py
+++ engine/engine.py
@@ -1,12 +1,14 @@
 import requests
 from django.http import JsonResponse
 from .MQTT import MQTTWriter
+from .open_weather import OpenWeather
 
 
 THRESHOLDS = {
     'potentiometer': 128,
     'photoresistor': 100,
     'temperature': 10,
+    'weather': [3, 7, 8]
 }
 
 class Engine:
@@ -14,57 +16,27 @@ class Engine:
         self.url = url
         self.comandi = comandi
         self.mqtt = MQTTWriter("127.0.0.1", 1883)
+        self.openweather = OpenWeather("Modena, Italia", "a4d0ee049787674041208b1744a3a95b")
     
     def manage_windows(self, state):
         data = {'state': state}
         ret = requests.post(self.url+"update_windows/", data=data)
         return ret
 
-    # data: {'potentiometer': potentiometer_data, 'photoresistor': photoresistor_data, 'temperature': temperature_data}
-
-    def process_potentiometer_data(self, data):
-        if data['potentiometer'] >= THRESHOLDS['potentiometer']:
-            # chiudi le finestre
-            self.mqtt.publish_general_message('close')
-            return self.manage_windows("closed")
-        elif data['potentiometer'] < THRESHOLDS['potentiometer']:
-            # apri le finestre
-            self.mqtt.publish_general_message('open')
-            return self.manage_windows("open")
-        return JsonResponse({'message': "Finestre aggiornate correttamente"})
-
-    def process_photoresistor_data(self, data):
-        if data['photoresistor'] <= THRESHOLDS['photoresistor']:
-            # chiudi le finestre
-            self.mqtt.publish_general_message('close')
-            return self.manage_windows("closed")
-        else:
-            # apri le finestre
-            self.mqtt.publish_general_message('open')
-            return self.manage_windows("open")
-
-    def process_temperature_data(self, data):
-        if data['temperature'] <= THRESHOLDS['temperature']:
-            # chiudi le finestre
-            self.mqtt.publish_general_message('close')
-            return self.manage_windows("closed")
-        else:
-            # apri le finestre
-            self.mqtt.publish_general_message('open')
-            return self.manage_windows("open")
+    # data: {'potentiometer': potentiometer_data, 'photoresistor': photoresistor_data}
 
     def process_data(self, data):
-        # TODO: gestire le priorità: quale sensore ha priorità più alta nella decisione?
-        ret_potentiometer = self.process_potentiometer_data(data)
-        ret_photoresistor = self.process_photoresistor_data(data)
-        ret_thermometer = self.process_thermometer_data(data)
-        if ret_potentiometer.status_code != 200:
-            return ret_potentiometer
-        elif ret_photoresistor != 200:
-            return ret_photoresistor
-        elif ret_thermometer != 200:
-            return ret_thermometer
-        return ret_potentiometer
+        data_openweather = self.openweather.get_data()
+        data['temperature'] = data_openweather['main']['temp'] - 273.15
+        data['weather_id'] = data_openweather['weather'][0]['id']
+        open_conditions =  (data['weather_id'] // 100 in THRESHOLDS['weather']) + data['temperature'] >= THRESHOLDS['temperature'] + data['potentiometer'] <= THRESHOLDS['potentiometer'] + data['photoresistor'] >= THRESHOLDS['photoresistor']
+        close_conditions = (data['weather_id'] // 100 not in THRESHOLDS['weather']) + data['temperature'] < THRESHOLDS['temperature'] + data['potentiometer'] > THRESHOLDS['potentiometer'] + data['photoresistor'] < THRESHOLDS['photoresistor']
+        if open_conditions >= 2:
+            self.mqtt.publish_general_message('open')
+            return self.manage_windows('open')
+        elif close_conditions >= 3:
+            self.mqtt.publish_general_message('close')
+            return self.manage_windows('closed')
     
     def update_window(self, device_name, pin, comando):
         self.mqtt.publish_specific_message(device_name, pin, comando)
