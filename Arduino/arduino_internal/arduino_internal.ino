a9d9e7a285e49e71840b9f702d4c795d111f274d
Matteo Pagliani <64635883+MatteoPagliani@users.noreply.github.com>
2022-03-15 14:45:01 +0100

    Update arduino_internal.ino

--- Arduino/arduino_internal/arduino_internal.ino
+++ Arduino/arduino_internal/arduino_internal.ino
@@ -13,11 +13,6 @@ bool button_value_nord;
 #define OPEN 0
 #define CLOSED 1
 
-<<<<<<< HEAD
-unsigned long lasttime;
-
-=======
->>>>>>> 0d94eea1054471e3dd6953f952d216cb4635472c
 void setup() {
   Serial.begin(9600);
   // Setup della comunicazione seriale con il bridge interno per ricevere i comandi per ogni servo
@@ -26,14 +21,6 @@ void setup() {
   // Setup dei push button dedicati all'azionamento manuale delle finestre (1 button per finestra)
   pinMode(button_sud_pin, INPUT);
   pinMode(button_nord_pin, INPUT);
-<<<<<<< HEAD
-  lasttime = millis();
-}
-
-void loop() {
-  if (millis() - lasttime > 5000) {
-	  lasttime = millis();
-=======
 }
 
 void loop() {
@@ -83,53 +70,29 @@ void loop() {
     }
   
   if (Serial.available() > 0) {
->>>>>>> 0d94eea1054471e3dd6953f952d216cb4635472c
 	  
 	  // Leggere dati dal bridge (seriale) e in base agli stati ricevuti o alla pressione del push button aprire o chiudere le due finestre
 	  // Dati ricevuti: stato per ogni servo (chiuso, aperto)
 	  int new_pos_nord = Serial.read();
 	  int new_pos_sud = Serial.read();
-<<<<<<< HEAD
-	  int pos_nord = finestra_nord.read();
-	  int pos_sud = finestra_sud.read();
-	  
-	  button_value_sud = digitalRead(button_sud_pin); // HIGH = button pressed (--> cambia stato della finestra sud), LOW = button released
-	  button_value_nord = digitalRead(button_nord_pin); // HIGH = button pressed (--> cambia stato della finestra nord), LOW = button released
-	    
-	  
-	  if (new_pos_nord == OPEN && (pos_nord == CLOSED || button_value_nord == HIGH)) {
-=======
 	    
 	  if (new_pos_nord == OPEN && pos_nord == CLOSED) {
->>>>>>> 0d94eea1054471e3dd6953f952d216cb4635472c
 	    for (; pos_nord < OPEN; pos_nord += 1) {
 	      finestra_nord.write(pos_nord);
 	      delay(15);
 	    }
-<<<<<<< HEAD
-	  } else if (new_pos_nord == CLOSED && (pos_nord == OPEN || button_value_nord == HIGH)) {
-=======
 	  } else if (new_pos_nord == CLOSED && pos_nord == OPEN) {
->>>>>>> 0d94eea1054471e3dd6953f952d216cb4635472c
 	    for (; pos_nord > CLOSED; pos_nord -= 1) {
 	      finestra_nord.write(pos_nord);
 	      delay(15);
 	    }
 	  }
-<<<<<<< HEAD
-	  if (new_pos_sud == OPEN && (pos_sud == CLOSED || button_value_sud == HIGH)) {
-=======
 	  if (new_pos_sud == OPEN && pos_sud == CLOSED) {
->>>>>>> 0d94eea1054471e3dd6953f952d216cb4635472c
 	    for (; pos_sud < OPEN; pos_sud += 1) {
 	      finestra_sud.write(pos_sud);
 	      delay(15);
 	    }
-<<<<<<< HEAD
-	  } else if (new_pos_sud == CLOSED && (pos_sud == OPEN || button_value_sud == HIGH)) {
-=======
 	  } else if (new_pos_sud == CLOSED && pos_sud == OPEN) {
->>>>>>> 0d94eea1054471e3dd6953f952d216cb4635472c
 	    for (; pos_sud > CLOSED; pos_sud -= 1) {
 	      finestra_sud.write(pos_sud);
 	      delay(15);
